27c7a8a9fceb15906276fab66c8a31b0fe4a6a5d
Nikita Suchkov <suchkov8520@gmail.com>
2023-07-25 15:04:33 +0300

    Del: css nano plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,3 @@
-const cssnano = require("cssnano");
 const isDev = process.env.NODE_ENV !== "production";
 
 module.exports = {
@@ -7,11 +6,5 @@ module.exports = {
       isDev ? undefined : require("autoprefixer"),
       isDev ? undefined : require("postcss-mq-keyframes"),
       isDev ? undefined : require("postcss-sort-media-queries"),
-      isDev
-         ? undefined
-         : cssnano([
-              "default",
-              { discardComments: { removeAll: true }, discardEmpty: true, discardDuplicates: true, minifyFontValues: true },
-           ]),
    ],
 };
