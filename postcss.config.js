3c66660eeef8d0aba6e5adeca67485b57aa2eb63
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-01 15:04:33 +0300

    Update: postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 const isDev = process.env.NODE_ENV !== "production";
+const autoprefixer = require("autoprefixer");
+const postcsssortmediaqueries = require("postcss-sort-media-queries");
 
 module.exports = {
    plugins: [
-      require("tailwindcss"),
-      isDev ? undefined : require("autoprefixer"),
-      isDev ? undefined : require("postcss-mq-keyframes"),
-      isDev ? undefined : require("postcss-sort-media-queries"),
+      autoprefixer,
+      postcsssortmediaqueries,
    ],
 };
