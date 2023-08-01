e797974d5cbbc64024a577bc3da3120dba6a9c3d
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-01 15:03:46 +0300

    Add: store default setting

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const useAppStore = defineStore("defaultStore", {
+   state: () => ({
+
+   }),
+
+   getters: {
+
+   },
+
+   actions: {
+
+   },
+});
