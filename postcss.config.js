a9f2a831331d82809633d7b38b5a52867aad14a6
Nikita Suchkov <suchkov8520@gmail.com>
2023-10-14 08:52:28 +0300

    Update postcss plugin and config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,32 @@
 const isDev = process.env.NODE_ENV !== "production";
+const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
-const postcsssortmediaqueries = require("postcss-sort-media-queries");
+const postcsssortmediaqueries = require("postcss-combine-media-query");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
 
 module.exports = {
    plugins: [
+      tailwindcss,
       autoprefixer,
       postcsssortmediaqueries,
+      cssnano(["default", { discardComments: { removeAll: true }, discardEmpty: true, discardDuplicates: true, minifyFontValues: true }]),
+      purgecss({
+         content: ["./src/**/*.vue"],
+         css: [],
+         dynamicAttributes: [""],
+         fontFace: false,
+         keyframes: false,
+         variables: true,
+         rejected: false,
+         rejectedCss: false,
+         safelist: {
+            standard: ["body", "html", "a", /\w+-(?:\[\d+(px|deg)\])?/],
+            deep: [],
+            greedy: [],
+            keyframes: [],
+            variables: [],
+         },
+      }),
    ],
 };
