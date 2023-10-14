d92e84962102ff7e60918fc65da5ad2243391694
Nikita Suchkov <suchkov8520@gmail.com>
2023-10-14 08:52:53 +0300

    Add pinia

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import { createApp } from "vue";
+import { createPinia } from 'pinia'
 import App from "./layouts/default.vue";
 import Components from "@/components/index";
 import Router from "@/router/router";
 import "./index.css";
 
 const app = createApp(App);
+const pinia = createPinia()
 
 Components.forEach((component) => {
    app.component(component.name, component);
 });
 
-app.use(Router).mount("#app");
+app.use(Router).use(pinia).mount("#app");
